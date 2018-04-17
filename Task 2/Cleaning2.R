ec8acfa2f8be14bf223a46a31c00356249c0f86b
Matt <35929803+matthannula@users.noreply.github.com>
2018-04-17 14:44:17 -0700

    Update Cleaning2

--- Task 2/Cleaning2.R
+++ Task 2/Cleaning2.R
@@ -4,7 +4,7 @@ setwd(filepath)
 #for Matt's computer: filepath<-"C:/Users/owner/Documents/ECON 386/Project 1/ECON386REPO/Data Cleaning Project/Task 2"
 library(dplyr)
 library(tidyr)
-#Create a vecotr of column names to be the header of the imported data. Unneeded columns will be titled delete_n
+#Create a vector of column names to be the header of the imported data. Unneeded columns will be titled delete_n
 column_names<-c("Plant_ID", "delete_1", "Year", "Electricity", "SO2", "NOx", "Capital", "Employees", "Coal_hc", "Oil_hc", "Gas_hc", "delete_2", "delete_3")  #create a vector of column names 
 #Upload data into R and simultaneously convert data.frame to tbl_df format
 power<-tbl_df(read.table("Panel_8595.txt", skip=2, col.names = column_names))
